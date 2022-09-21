e4838897224a07ecc375db5a7afdf3f658e62af3
정세욱 <tpdnrqkqh@gmail.com>
2022-09-21 13:58:00 +0900

    [ADD] Separated apps

--- config/settings.py
+++ config/settings.py
@@ -30,14 +30,26 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+] 
+
+PROJECT_APPS = [
+    "context"
 ]
+
+THIRD_PARTY_APPS = [
+    "django",
+    "rest_framework"
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
