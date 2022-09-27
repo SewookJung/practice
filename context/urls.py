edaac44913f79bfc886c19b283f481cf13852600
정세욱 <tpdnrqkqh@gmail.com>
2022-09-27 16:41:33 +0900

    [FIX] Clean up by black formatter

--- context/urls.py
+++ context/urls.py
@@ -5,6 +5,10 @@ from .views import ProvideContextView, NotProvideContextView, CustomContextView
 
 urlpatterns = [
     path("context/provide", ProvideContextView.as_view(), name="provide_context"),
-    path("context/not/provide", NotProvideContextView.as_view(), name="not_provide_context"),
+    path(
+        "context/not/provide",
+        NotProvideContextView.as_view(),
+        name="not_provide_context",
+    ),
     path("context/custom", CustomContextView.as_view(), name="custom_context"),
 ]
