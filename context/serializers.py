73330c0d3d9c5483bdb877945e0f0ff979d7ba72
정세욱 <tpdnrqkqh@gmail.com>
2022-09-27 16:28:16 +0900

    [FIX] Clean up by pre commit

--- context/serializers.py
+++ context/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import Serializer, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer
 
 from .models import Context
 
@@ -12,35 +12,29 @@ class ProvideContextSerializer(ModelSerializer):
         ModelSerializer (_type_): _description_
     """
 
-    
     def get_field_names(self, declared_fields, info):
         context = self.context
-        print(context) # {"request": value ,"view": value, "format": value}
+        print(context)  # {"request": value ,"view": value, "format": value}
 
-        
         if self.context:
             request = self.context.get("request", None)
-            
+
             if request and hasattr(request, "user"):
                 user = request.user
                 print(user)
 
         return super().get_field_names(declared_fields, info)
-    
 
     class Meta:
         model = Context
         fields = "__all__"
-    
 
-    
+
 class NotProvideContextSerializer(ModelSerializer):
-
     def get_field_names(self, declared_fields, info):
         context = self.context
-        print(context) # {}
+        print(context)  # {}
         return super().get_field_names(declared_fields, info)
-    
 
     class Meta:
         model = Context
@@ -48,12 +42,11 @@ class NotProvideContextSerializer(ModelSerializer):
 
 
 class CustomContextSerializer(ModelSerializer):
-
     def get_field_names(self, declared_fields, info):
         context = self.context
         print(context)
         return super().get_field_names(declared_fields, info)
-    
+
     class Meta:
         model = Context
         fields = "__all__"
