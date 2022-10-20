0f700d8873bbc161baafdb9b7f0d1e7ae468be2b
정세욱 <tpdnrqkqh@gmail.com>
2022-10-20 16:43:19 +0900

    add Orm prefetch view for test

--- orm/views.py
+++ orm/views.py
@@ -1,17 +1,18 @@
 from django.db import connection
+from django.db.models import Prefetch
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from .models import Pizza
+from .models import Pizza, Topping
 
 
 class OrmWithDatabaseView(generics.GenericAPIView):
-    """PrefetchView Prefect function
+    """PrefetchView Evaluated
 
-    Checkout database hit by orm
+    Check queryset evaluated by orm
 
     Check Point:
-        - Database hit point
+        - Check database hit count
     """
 
     def get(self, request, *args, **kwargs):
@@ -20,32 +21,129 @@ class OrmWithDatabaseView(generics.GenericAPIView):
         Check out database hit point by django orm
 
         Object total count
-         - Pizza: 2
-         - Topping: 3
+         - Pizza: 4
+         - Topping: 6
         """
 
-        # Database hit
-        pizza_by_get_func = Pizza.objects.get(id=1)
+        # Evaluated
+        pizza_search_by_get = Pizza.objects.get(id=1)
         # output: 1
-        print(pizza_by_get_func)
         print(len(connection.queries))
-
-        # No database hit
-        pizza_by_all_func = Pizza.objects.all()
-        # output: 0
-        print(len(connection.queries))
-        # Database hit
-        print(pizza_by_all_func)
+        print(pizza_search_by_get)
         # output: 1
         print(len(connection.queries))
 
-        # No database hit
-        pizza_by_filtering = Pizza.objects.filter(id=1)
+        # Not evaluated
+        pizza_search_by_all = Pizza.objects.all()
         # output: 0
         print(len(connection.queries))
-        # Database hit
-        print(pizza_by_filtering)
+        # Evaluated
+        print(pizza_search_by_all)
         # output: 1
         print(len(connection.queries))
+
+        # Not evaluated
+        pizza_search_by_filter = Pizza.objects.filter(id=1)
+        # output: 0
+        print(len(connection.queries))
+        # Evaluated
+        print(pizza_search_by_filter)
+        # output: 1
+        print(len(connection.queries))
+
+        return Response(data=None, status=status.HTTP_200_OK)
+
+
+class OrmPrefetchView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        """Case 1
+        Not used prefetch of objects
+
+        Object total count
+         - Pizza: 4
+         - Topping: 6
+        """
+
+        pizza = Pizza.objects.all()  # Not evaluated
+
+        for item in pizza:  # Evaluated & output: 1
+            print(item)
+            toppings = item.topping.all()  # Not evaluated
+            print(toppings)  # Evaluated
+
+        print(len(connection.queries))
+
+        """Case 2
+        Used prefetch get filter
+        """
+
+        pizza_by_related = Pizza.objects.prefetch_related("topping").get(
+            name="불고기 피자"
+        )  # Evaluated
+        print(len(connection.queries))  # Database hit count: 2
+
+        toppings = pizza_by_related.topping.all()  # Not evaluated
+        list(toppings)  # Force evaluated & Not evaludated
+        print(len(connection.queries))  # Database hit count: 2
+
+        """Case 3
+        Used prefetch all filter
+        """
+
+        pizza_by_related = Pizza.objects.all().prefetch_related(
+            "topping"
+        )  # Not evaluated
+
+        print(len(connection.queries))  # Database hit count: 0
+
+        for item in pizza_by_related:  # Evaluated
+            toppings = item.topping.all()
+            for topping in toppings:
+                print(topping)
+            print("Evaluated", item)
+
+        print(len(connection.queries))  # Database hit count: 2
+
+        for item in pizza_by_related:  # Not evaluated
+            print("Cached", item)
+
+        for item in pizza_by_related:  # Not evaluated
+            toppings = item.topping.all()
+
+            for topping in toppings:  # Not evaluated
+                print(topping)
+
+        print(len(connection.queries))  # Database hit count: 2
+
+        """Case 4
+        Related model filtering with prefetch object
+        """
+
+        pizza_by_related = Pizza.objects.all().prefetch_related(
+            Prefetch(
+                "topping",
+                queryset=Topping.objects.filter(name="올리브"),
+                to_attr="olive_topping",
+            )
+        )
+
+        for pizza in pizza_by_related:
+            print(pizza, end=" ")
+            olive_topping = pizza.olive_topping
+            print(olive_topping)
+
+        # """Case 5
+        # Bad referance get related model
+        # """
+
+        pizza_by_related = Pizza.objects.prefetch_related(
+            "topping"
+        ).all()  # Not evaluated
+        print(len(connection.queries))  # Database hit count: 0
+
+        for item in pizza_by_related:  # Evaluated
+            toppings = item.topping.filter(name="올리브")
+            for topping in toppings:
+                print(topping)
 
         return Response(data=None, status=status.HTTP_200_OK)
