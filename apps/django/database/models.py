e711db84354c269e71f8980a61e8780faafdf13b
정세욱 <tpdnrqkqh@gmail.com>
2022-12-04 00:10:44 +0900

    test(discussion): for use in discussion code

--- apps/django/database/models.py
+++ apps/django/database/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class TestUuid(models.Model):
-    uuid = models.UUIDField(verbose_name="uuid")
+    uuid = models.UUIDField(unique=True, editable=True, verbose_name="uuid")
