98ec1df166a1e0cca9523c49f028efc335817794
정세욱 <tpdnrqkqh@gmail.com>
2023-02-08 00:09:48 +0900

    test: model field of editable option

--- apps/django/database/serializers.py
+++ apps/django/database/serializers.py
@@ -4,6 +4,9 @@ from .models import TestUuid
 
 
 class UuidSerializer(serializers.ModelSerializer):
+    uuid = serializers.UUIDField(help_text="uuid")
+
     class Meta:
         model = TestUuid
         fields = "__all__"
+        extra_kwargs = {"uuid": {"write_only": True}}
